55a7a090b04a29e97bbd8910f163438b43d1c7ce
Azzubair <178708736+azzubairx@users.noreply.github.com>
2026-06-12 16:27:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // مهم جداً للنشر على GitHub Pages. قم بتغيير subnirvana لاسم مستودعك.
-  base: '/subnirvana/', 
+  // احذف الـ base أثناء التطوير المحلي، وأعده فقط عند رفع المشروع لـ Github Pages
+  // base: '/subnirvana/', 
 })
